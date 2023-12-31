7fa2d63ec53d9f3c8ee05397d479664ac76283bd
WinnardArthur <winnardarthur7@gmail.com>
2023-12-31 12:39:38 +0000

    create billboard section

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,19 @@
-const HomePage = () => {
-  return <div>Hello Store</div>;
+import getBillboard from "@/actions/getBillboard";
+import { Billboard } from "@/components/billboard";
+import { Container } from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard("6590249d8e0fac58790b7bbd");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
