67ba062182a28dcc8e26fb8d16f0956107e89db6
WinnardArthur <winnardarthur7@gmail.com>
2023-12-31 16:16:45 +0000

    fetch and display products

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,16 +1,23 @@
 import getBillboard from "@/actions/getBillboard";
+import getProducts from "@/actions/getProducts";
 import { Billboard } from "@/components/billboard";
+import { ProductList } from "@/components/productList";
 import { Container } from "@/components/ui/container";
 
 export const revalidate = 0;
 
 const HomePage = async () => {
   const billboard = await getBillboard("6590249d8e0fac58790b7bbd");
+  const products = await getProducts({ isFeatured: true });
 
   return (
     <Container>
       <div className="space-y-10 pb-10">
         <Billboard data={billboard} />
+
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
       </div>
     </Container>
   );
---
+++ components/ui/currency.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { formatter } from "@/lib/utils";
+import React, { useEffect, useState } from "react";
+
+type CurrencyProps = {
+  value?: string | number;
+};
+
+export const Currency = ({ value }: CurrencyProps) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
+
+  return <div className="font-semibold">{formatter.format(Number(value))}</div>;
+};
