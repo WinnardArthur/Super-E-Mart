286641217c08d5476285773120812d7c45ff3d25
WinnardArthur <winnardarthur7@gmail.com>
2024-01-01 09:42:02 +0000

    add mobile filters for products

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -8,6 +8,7 @@ import React from "react";
 import Filter from "./components/filter";
 import NoResults from "@/components/ui/no-results";
 import { ProductCard } from "@/components/ui/product-card";
+import MobileFilters from "./components/mobileFilter";
 
 export const revalidate = 0;
 
@@ -37,6 +38,7 @@ const CategoryPage = async ({ params, searchParams }: CategoryPageProps) => {
       <Container>
         <Billboard data={category.billboard} />
         <div className="px-4 sm:px-6 lg:px-8 pb-24">
+          <MobileFilters sizes={sizes} colors={colors} />
           <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
             <div className="hidden lg:block">
               <Filter name="Sizes" data={sizes} valueKey="sizeId" />
